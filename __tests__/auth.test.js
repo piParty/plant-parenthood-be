3c0838a9492462cb34260feac6046d0157092d83
Tess Lameyer <tjlameyer@gmail.com>
2020-01-13 13:54:33 -0800

    updated fields and model tests

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,20 +1,13 @@
 require('dotenv').config();
 
+const { getUser } = require('../lib/helpers/data-helpers');
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
+
 
 describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
 
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
+
+
 });
