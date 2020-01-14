aa7cbac26805348a55107e73f20df04bdcdb141e
Tess Lameyer <tjlameyer@gmail.com>
2020-01-13 17:02:56 -0800

    tests for login failure due to a bad email or a bad password are passing

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -40,34 +40,34 @@ describe('app routes', () => {
       });
   });
 
-  // it('fails to login a user with a bad email', async() => {
-  //   await getUser();
+  it('fails to login a user with a bad email', async() => {
+    await getUser();
 
-  //   return request(app)
-  //     .post('/api/v1/auth/login')
-  //     .send({ email: 'badEmail@notgood.io', password: 'password' })
-  //     .then(res => {
-  //       expect(res.status).toEqual(401);
-  //       expect(res.body).toEqual({
-  //         status: 401,
-  //         message: 'Invalid Email or Password'
-  //       });
-  //     });
-  // });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'badEmail@notgood.io', password: 'password' })
+      .then(res => {
+        expect(res.status).toEqual(401);
+        expect(res.body).toEqual({
+          status: 401,
+          message: 'Invalid Email or Password'
+        });
+      });
+  });
 
-  // it('fails to login a user with a bad password', async() => {
-  //   await getUser();
+  it('fails to login a user with a bad password', async() => {
+    await getUser();
 
-  //   return request(app)
-  //     .post('/api/v1/auth/login')
-  //     .send({ email: 'user@tess.com', password: 'notright' })
-  //     .then(res => {
-  //       expect(res.status).toEqual(401);
-  //       expect(res.body).toEqual({
-  //         status: 401,
-  //         message: 'Invalid Email or Password'
-  //       });
-  //     });
-  // });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'user@tess.com', password: 'notright' })
+      .then(res => {
+        expect(res.status).toEqual(401);
+        expect(res.body).toEqual({
+          status: 401,
+          message: 'Invalid Email or Password'
+        });
+      });
+  });
 
 });
