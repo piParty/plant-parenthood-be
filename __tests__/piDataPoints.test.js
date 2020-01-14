af7888494fa173aecf87004a517d08ce551d16b1
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-01-13 16:56:50 -0800

    verify pi token and test

---
+++ __tests__/piDataPoints.test.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { getUser } = require('../lib/helpers/data-helpers');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('app routes', () => {
+  it('should be able to verify a session and post a data point', async() => {
+    return request(app)
+      .post('/dataPoints', {
+        averageValue: 4,
+        standardDeviation: 1,
+        piTimestamp: Date.now()
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          averageValue: 4,
+          standardDeviation: 1,
+          piTimestamp: res.piTimestamp
+        });
+      });
+  
+  });
+});
