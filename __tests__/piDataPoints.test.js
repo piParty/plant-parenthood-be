4bc03414367fbb2e2b10e045e7bdfe4f6c4fe940
Tess Lameyer <tjlameyer@gmail.com>
2020-01-15 10:42:59 -0800

    preparing to pull in fresh code from Ian branch

--- __tests__/piDataPoints.test.js
+++ __tests__/piDataPoints.test.js
@@ -1,23 +1,42 @@
 require('dotenv').config();
 
-const { getPiDataPoint } = require('../lib/helpers/data-helpers');
+const { getPiDataPoint, getPiDataSession, userAgent } = require('../lib/helpers/data-helpers');
 const request = require('supertest');
 const app = require('../lib/app');
 
 
-describe('app routes', () => {
-  it('should be able to verify a session and post a data point', () => {
+describe('piDataPoint route tests', () => {
+  it('(the pi) should be able to verify a session and post a data point using this route', async() => {
+    const session = await getPiDataSession()
+      // .then(session => console.log(session.cookies, 'this is the data session cookie'))
+    
+    console.log(session, 'this is the sessionnnnnnnnnn')
+
+    userAgent
+    //to make sure that the agent gets assigned a data session cookie!
+      .post('/api/v1/pi-data-sessions')
+    
     return request(app)
-      .post('/dataPoints', {
-        averageValue: 4,
-        standardDeviation: 1,
+      .post('/api/v1/data-points') 
+      .send({
+        piDataSessionId: session._id,
+        data: {
+          averageValue: 10, 
+          standardDeviation: 2
+        },
         piTimestamp: Date.now()
       })
       .then(res => {
-        expect(res.status).toEqual(404);
+        // expect(res.header['set-cookie'][0]).toEqual(expect.stringContaining('session='));
         expect(res.body).toEqual({
-          status: 404,
-          message: 'Not Found'
+          _id: expect.any(String),
+          piDataSessionId: session._id.toString(),
+          data: {
+            averageValue: 10, 
+            standardDeviation: 2
+          },
+          piTimestamp: Date.now(),
+          __v: 0 
         });
       });
   });
