bb170bb7283e471b0d72ab315c4a9712f8c23447
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-01-16 21:37:11 -0800

    successfully seed plant database with lowlight plants

--- plant-seed-data.js
+++ plant-seed-data.js
@@ -1,2 +1,17 @@
-const plants = require('./environment/plants.json');
-console.log(plants);
+require('dotenv').config();
+const plants = require('./plants.json');
+const Plant = require('./lib/models/Plant');
+require('./lib/utils/connect')();
+const mongoose = require('mongoose');
+
+const plantObjectsArray = plants.low_light_plants.map(plantName => {
+  return ({
+    commonName: plantName,
+    sunlightPreference: 'low'
+  });
+});
+
+const seedPlants = async() => await Plant.create(plantObjectsArray)
+  .then(() => mongoose.connection.close());
+
+seedPlants();
