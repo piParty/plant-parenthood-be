aee71340a8a9bb5955f8cc8bfa6716d73b565fb8
Tess Lameyer <tjlameyer@gmail.com>
2020-01-19 17:48:34 -0800

    console logs all over test, still a 404

--- __tests__/userAggregations.test.js
+++ __tests__/userAggregations.test.js
@@ -8,22 +8,60 @@ describe('user aggregation route tests', () => {
   describe('piDataSession tests for getting all data sessions by user', () => {
     it('should be able to get all piDataSessions by user', async() => {
       const user = await getUser({ email: 'user0@tess.com' });
+      console.log(user, 'user');
 
       let dataSessionId1;
       let dataSessionId2;
       return userAgent
       //agent must post a session to get dataSession token before pi can post any data points
-        .post('/api/v1/user-pi-data-sessions')
-        .send({ piNickname: 'happyPi', sensorType: 'light', piLocationInHouse: 'kitchen', city: 'Portland' })
+        //posting first session
+        .post('/api/v1/pi-data-sessions')
+        .send({ 
+          piNicknameId: user.myPis[0]._id, 
+          sensorType: 'light', 
+          piLocationInHouse: 'kitchen', 
+          city: 'Portland'
+        })
         .then(res => {
+          console.log(res.body, 'this is the first body');
           dataSessionId1 = res.body._id;
+          console.log(res.body._id, 'this is the id');
           return userAgent
-          //hitting aggregation route
-            .get(`/api/v1/user-pi-data-sessions/${user._id}`)
+          //posting second session
+            .post('/api/v1/pi-data-sessions')
+            .send({ 
+              piNicknameId: user.myPis[1]._id, 
+              sensorType: 'light', 
+              piLocationInHouse: 'kitchen', 
+              city: 'Portland' 
+            })
             .then(res => {
               dataSessionId2 = res.body._id;
-              console.log(dataSessionId1, dataSessionId2);
-              expect(res.body).toEqual([{}, {}]);
+              return userAgent
+              //hitting aggregation route
+                .get(`/api/v1/auth/user-pi-data-sessions/${user._id}`)
+                .then(res => {
+                  console.log(res.body, 'this is the body');
+                  console.log(dataSessionId1, dataSessionId2);
+                  //expecting two sessions
+                  expect(res.body).toEqual([{
+                    _id: expect.any(String), 
+                    piNicknameId: user.piNickname[0]._id, 
+                    sensorType: 'light',
+                    piLocationInHouse: 'kitchen',
+                    city: 'Portland',
+                    __v: 0,
+                    dataSession: expect.any(String)
+                  }, {
+                    _id: expect.any(String), 
+                    piNicknameId: user.piNickname[1]._id, 
+                    sensorType: 'light',
+                    piLocationInHouse: 'kitchen',
+                    city: 'Portland',
+                    __v: 0,
+                    dataSession: expect.any(String)
+                  }]);
+                });
             });
         });
     });
@@ -41,4 +79,4 @@ describe('user aggregation route tests', () => {
 //     });
 // });
 
-// });
+
