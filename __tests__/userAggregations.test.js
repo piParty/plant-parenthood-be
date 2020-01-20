0c710b7607d7d1d5da3b97a36b92e71bf8b1210f
Tess Lameyer <tjlameyer@gmail.com>
2020-01-19 16:46:18 -0800

    reimagined test and currently getting a 404, time for TDD

--- __tests__/userAggregations.test.js
+++ __tests__/userAggregations.test.js
@@ -5,8 +5,40 @@ const app = require('../lib/app');
 
 
 describe('user aggregation route tests', () => {
-  it('can aggregate all data sessions by user', () => {
-   
-  });
+  describe('piDataSession tests for getting all data sessions by user', () => {
+    it('should be able to get all piDataSessions by user', async() => {
+      const user = await getUser({ email: 'user0@tess.com' });
 
+      let dataSessionId1;
+      let dataSessionId2;
+      return userAgent
+      //agent must post a session to get dataSession token before pi can post any data points
+        .post('/api/v1/user-pi-data-sessions')
+        .send({ piNickname: 'happyPi', sensorType: 'light', piLocationInHouse: 'kitchen', city: 'Portland' })
+        .then(res => {
+          dataSessionId1 = res.body._id;
+          return userAgent
+          //hitting aggregation route
+            .get(`/api/v1/user-pi-data-sessions/${user._id}`)
+            .then(res => {
+              dataSessionId2 = res.body._id;
+              console.log(dataSessionId1, dataSessionId2);
+              expect(res.body).toEqual([{}, {}]);
+            });
+        });
+    });
+  });
 });
+
+// it('should throw an error if a user tries to do this aggregation without being logged in', () => {
+//   return request(app)
+//     .get('/api/v1/pi-data-sessions/')
+//     .then(res => {
+//       expect(res.body).toEqual({
+//         message: 'Login required.',
+//         status: 403
+//       });
+//     });
+// });
+
+// });
